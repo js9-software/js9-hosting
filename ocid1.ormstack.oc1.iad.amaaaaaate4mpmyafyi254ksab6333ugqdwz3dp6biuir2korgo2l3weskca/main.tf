5f84d2ffe886dc7c4b2e2f62c7c8787cbaa981dc
Jeff Amaral <jeff@freethoughtdesign.com>
2025-07-17 08:27:15 -0400

    Initial commit

---
+++ ocid1.ormstack.oc1.iad.amaaaaaate4mpmyafyi254ksab6333ugqdwz3dp6biuir2korgo2l3weskca/main.tf
@@ -0,0 +1,74 @@
+provider "oci" {}
+
+resource "oci_core_instance" "generated_oci_core_instance" {
+	agent_config {
+		is_management_disabled = "false"
+		is_monitoring_disabled = "false"
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Vulnerability Scanning"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Management Agent"
+		}
+		plugins_config {
+			desired_state = "ENABLED"
+			name = "Custom Logs Monitoring"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute RDMA GPU Monitoring"
+		}
+		plugins_config {
+			desired_state = "ENABLED"
+			name = "Compute Instance Monitoring"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute HPC RDMA Auto-Configuration"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Compute HPC RDMA Authentication"
+		}
+		plugins_config {
+			desired_state = "ENABLED"
+			name = "Cloud Guard Workload Protection"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Block Volume Management"
+		}
+		plugins_config {
+			desired_state = "DISABLED"
+			name = "Bastion"
+		}
+	}
+	availability_config {
+		recovery_action = "RESTORE_INSTANCE"
+	}
+	availability_domain = "UupJ:US-ASHBURN-AD-1"
+	compartment_id = "ocid1.tenancy.oc1..aaaaaaaa6a3keobrhqzfmxkznyvr7s5uhrrhkyujqr6d3sw7udmesxdylg3a"
+	create_vnic_details {
+		assign_ipv6ip = "false"
+		assign_private_dns_record = "true"
+		assign_public_ip = "true"
+		subnet_id = "ocid1.subnet.oc1.iad.aaaaaaaa6erykkuzhn5rhr7dqni5frlxod3elqkn74lizfphqvilgkpxbr6q"
+	}
+	display_name = "js9-x86-hosting"
+	instance_options {
+		are_legacy_imds_endpoints_disabled = "false"
+	}
+	is_pv_encryption_in_transit_enabled = "true"
+	metadata = {
+		"ssh_authorized_keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINP+spWvrIK/mraidaql+GsAzTBSsSxL75B/uq+xJIzu jeff+theseus_2025@freethoughtdesign.com"
+	}
+	shape = "VM.Standard.E2.1.Micro"
+	source_details {
+		boot_volume_size_in_gbs = "50"
+		boot_volume_vpus_per_gb = "10"
+		source_id = "ocid1.image.oc1.iad.aaaaaaaapmcqm63rd6vdbcztfowph4ffgfu6dsvlj3gl5w6dvt5hfvgk3mfa"
+		source_type = "image"
+	}
+}
